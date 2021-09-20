63bccafde9ab46e724696e08b48bdb543207d979
Triana Cerda <trianaRcerda@gmail.com>
2021-09-20 16:22:58 -0700

    green test for GET / for contents of index.html

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,8 @@
 const request = require('supertest');
 const app = require('../lib/app'); //check path issue?
-const { writeFile, readFile, readdir, rm } = require('fs/promises');
+// const { writeFile, readFile, readdir, rm } = require('fs/promises');
 
 describe('http routes', () => {
-  const rootDir = '../public';
-
   it('should return contents in index.html from GET /', async () => {
     const res = await request(app).get('/');
     expect(res.text).toEqual(expect.any(String));
