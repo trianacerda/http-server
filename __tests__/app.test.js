0af522c7f654996b7838e43de73c537d61be542c
Triana Cerda <trianaRcerda@gmail.com>
2021-09-20 17:13:09 -0700

    red failing / GET for index.html

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,19 @@
 const request = require('supertest');
 const app = require('../lib/app'); //check path issue?
-// const { writeFile, readFile, readdir, rm } = require('fs/promises');
+const { readFile } = require('fs/promises');
 
 describe('http routes', () => {
-  it('should return contents in index.html from GET /', async () => {
-    const res = await request(app).get('/');
-    expect(res.text).toEqual(expect.any(String));
+  it.only('should return contents in index.html from GET /', async () => {
+    const [res, file] = await Promise.all([
+      request(app).get('/'),
+      readFile('./public/index.html', 'utf-8'),
+    ]);
+
+    expect(res.text).toEqual(file);
   });
+
+  // it('should return main.css file contents on GET /styles/main.css', async () => {
+  //   const res = await request(app).get('/css/main.css');
+  //   expect(res.text).toEqual(expect.any(String));
+  // });
 });
