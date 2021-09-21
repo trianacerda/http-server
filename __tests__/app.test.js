6dfea8a9dc80d688cbfc2c58b72f007aeb5c85f2
Triana Cerda <trianaRcerda@gmail.com>
2021-09-20 17:29:40 -0700

    red failing for GET /css/ main.css

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const app = require('../lib/app'); //check path issue?
 const { readFile } = require('fs/promises');
 
 describe('http routes', () => {
-  it.only('should return contents in index.html from GET /', async () => {
+  it('should return contents in index.html from GET /', async () => {
     const [res, file] = await Promise.all([
       request(app).get('/'),
       readFile('./public/index.html', 'utf-8'),
@@ -12,8 +12,12 @@ describe('http routes', () => {
     expect(res.text).toEqual(file);
   });
 
-  // it('should return main.css file contents on GET /styles/main.css', async () => {
-  //   const res = await request(app).get('/css/main.css');
-  //   expect(res.text).toEqual(expect.any(String));
-  // });
+  it('should return main.css file contents on GET /styles/main.css', async () => {
+    const [res, file] = await Promise.all([
+      request(app).get('/css/main.css'),
+      readFile('./public/css/main.css', 'utf-8'),
+    ]);
+
+    expect(res.text).toEqual(file);
+  });
 });
