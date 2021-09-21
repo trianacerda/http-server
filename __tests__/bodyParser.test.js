6e5c783615b67b29c270c0e77a80c44b2e9074ef
Triana Cerda <trianaRcerda@gmail.com>
2021-09-21 15:39:09 -0700

    red failing test for return null if not PATCH, PUT, POST

--- __tests__/bodyParser.test.js
+++ __tests__/bodyParser.test.js
@@ -1,10 +1,13 @@
-const request = require('supertest');
+// const request = require('supertest');
 const bodyParser = require('../lib/bodyParser');
 // const { readFile } = require('fs/promises');
 
 describe('Body Parser', () => {
   it('returns null if method is not POST, PUT, or PATCH', async () => {
-    const method = request(bodyParser).get('/');
-    expect(method).toBeNull();
+    const testObj = bodyParser({
+      method: 'GET',
+      url: '/dogs/2',
+    });
+    expect(testObj.method).toBeNull();
   });
 });
